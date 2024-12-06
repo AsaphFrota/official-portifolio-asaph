221be505b6f9c31551397a5741dde7a794685744
asaph <af.asaph+dev@gmail.com>
2024-12-05 22:53:39 -0300

    feat: portifolio 95% completo

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,26 +1,29 @@
-import React, { useState } from "react";
-import { navLinks } from "../constants/index";
+import { useState } from "react";
 
-const NavItems = () => {
-  return (
-    <ul className="nav-ul">
-      {navLinks.map(({ id, href, name }) => (
-        <li key={id} className="nav-li">
-          <a href={href} className="nav-li_a" onClick={() => {}}>
-            {name}
-          </a>
-        </li>
-      ))}
-    </ul>
-  );
-};
+import { navLinks } from "../constants/index.js";
+
+// eslint-disable-next-line react/prop-types
+const NavItems = ({ onClick = () => {} }) => (
+  <ul className="nav-ul">
+    {navLinks.map((item) => (
+      <li key={item.id} className="nav-li">
+        <a href={item.href} className="nav-li_a" onClick={onClick}>
+          {item.name}
+        </a>
+      </li>
+    ))}
+  </ul>
+);
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const toggleMenu = () => setIsOpen((prevIsOpen) => ![prevIsOpen]);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+  const closeMenu = () => setIsOpen(false);
 
   return (
-    <header className="fixed top-0 left-0 right-0 bg-black/90">
+    //className="fixed top-4 left-1/2 transform -translate-x-1/2 w-[90%] max-w-7xl bg-white/30 backdrop-blur-lg rounded-[30px] px-6 py-3 flex justify-between items-center shadow-lg">
+    <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
       <div className="max-w-7xl mx-auto">
         <div className="flex justify-between items-center py-5 mx-auto c-space">
           <a
@@ -29,25 +32,28 @@ const Navbar = () => {
           >
             Asaph
           </a>
+
           <button
             onClick={toggleMenu}
-            className="text-neutra-400 hover:text-white focus:outline-none sm:hidden flex"
+            className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex"
             aria-label="Toggle menu"
           >
             <img
               src={isOpen ? "assets/close.svg" : "assets/menu.svg"}
-              alt="toggledd"
+              alt="toggle"
               className="w-6 h-6"
             />
           </button>
+
           <nav className="sm:flex hidden">
             <NavItems />
           </nav>
         </div>
       </div>
+
       <div className={`nav-sidebar ${isOpen ? "max-h-screen" : "max-h-0"}`}>
         <nav className="p-5">
-          <NavItems />
+          <NavItems onClick={closeMenu} />
         </nav>
       </div>
     </header>
